b3bde0a877759577f527ced5cbc76689caed3a92
sofiadb <barreirosofia@hotmail.com>
2019-08-07 23:57:43 -0300

    4th commit

---
+++ nuevaapp2/build/precache-manifest.5d27b4b24cbe7050c12af4123521355c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f24762a2081f95596e916416217504",
+    "url": "/SofiaBarreiro/nuevoRepoCursoReact/index.html"
+  },
+  {
+    "revision": "c31df65acd28130ca7f7",
+    "url": "/SofiaBarreiro/nuevoRepoCursoReact/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/SofiaBarreiro/nuevoRepoCursoReact/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "c31df65acd28130ca7f7",
+    "url": "/SofiaBarreiro/nuevoRepoCursoReact/static/js/main.1b26a7fd.chunk.js"
+  },
+  {
+    "revision": "fceaf09d0e2f37e49216",
+    "url": "/SofiaBarreiro/nuevoRepoCursoReact/static/js/runtime~main.f07732bd.js"
+  }
+]);
